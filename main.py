b5cf2d3d23893f4e383e8b479644d0309ac6f82c
dimentio233 <johnnyhe7@outlook.com>
2023-12-03 03:16:44 -0800

    trying host 0,0,0,0

--- main.py
+++ main.py
@@ -147,5 +147,5 @@ def ask_openai():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 33507))
-    app.run(debug=False, port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
