fb96ac10f6ef44a9a060a306ef05ab52b5abaa74
dimentio233 <johnnyhe7@outlook.com>
2023-12-09 21:45:58 -0800

    changed port to 8000 for AWS nginx default port.

--- main.py
+++ main.py
@@ -149,5 +149,5 @@ def ask_openai():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
